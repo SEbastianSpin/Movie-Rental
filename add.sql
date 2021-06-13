e0efa5e4e6f3bff592ff82f9d0b48608c34fb257
Sebastian Espin <sebasespinalejo@gmail.com>
2021-06-13 23:18:01 +0200

    progress in databese coneection and ejs rendering  plus adding movies ready

---
+++ add.sql
@@ -0,0 +1,18 @@
+INSERT INTO MOVIES (MOVIE_TITLE, RELEASE_DATE , PRICE, RATING, GENRE)
+VALUES
+( 'Superman vs Batman', '2016.03.25',3, 3.5,'action'),
+  ( 'Deadpool', '2016.02.12',3,4.5,'action'),
+  ( 'Furious 7', '2015.04.03',3,4 ,'action'),
+  ( 'PK', '2014.12.19',4,2 ,'adventure'),
+  ( 'Gladiator', '2000.05.05', 2,5,'action'),
+  ( 'The Hangover', '2009.06.05',3,4.8,'comedy'),
+  ( '3 Idiots', '2009.12.25', 2, 4.2, 'comedy'),
+  ( 'Spectre', '2015.11.06', 2, 2, 'action'),
+  ( 'Batman Begins', '2005.06.15',3, 5,'action' ),
+  ( 'The Dark Knight', '2008.07.18', 3,4.9,'action'),
+   ('The Lord of the Rings: The Return of the King', '2003.01.18', 5,4, 'fantasy'),
+   ('The Hobbit: The Battle of the Five Armies', '2014.01.18', 4,4, 'fantasy'),
+  ('The Lord of the Rings: The Two Towers', '2002.01.18', 3.5,4, 'fantasy'),
+  ('The Lord of the Rings: The Fellowship of the Ring', '2001.01.18', 4,4, 'fantasy'),
+  ('Harry Potter and Goblet of Fire', '2005.01.18', 4.5,6, 'fantasy');
+  
--- index.js
+++ index.js
@@ -1,41 +1,58 @@
+//jshint esversion:6
 var express = require('express');
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const sql = require("mssql");
 var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+// config for your database
+var config = {
+    user: 'test123',
+    password: 'test123',
+    server: 'localhost',
+
+    options: {
+encrypt: false, // for azure
+trustServerCertificate: true // change to true for local dev / self-signed certs
+}
+};
+
+
+// connect to your database
+sql.connect(config, function (err) {
+
+    if (err) console.log(err);
+
+
+
+
+});
 
 app.get('/', function (req, res) {
 
-    var sql = require("mssql");
+  // create Request object
+  var request = new sql.Request();
+  // query to the database and get the records
+  request.query('use CHOLOFLIX ;select * from MOVIES', function (err, recordset) {
 
-    // config for your database
-    var config = {
-        user: 'test123',
-        password: 'test123',
-        server: 'localhost',
-        database: 'HERTZ',
-        options: {
-    encrypt: false, // for azure
-    trustServerCertificate: true // change to true for local dev / self-signed certs
-  }
-    };
+      if (err) console.log(err);
+ 
+      // send records as a response
+      res.render("home",{
+        movie: recordset.recordsets[0][0]
+      });
 
-    // connect to your database
-    sql.connect(config, function (err) {
+  });
 
-        if (err) console.log(err);
-
-        // create Request object
-        var request = new sql.Request();
-
-        // query to the database and get the records
-        request.query('use HERTZ ;select * from CARS', function (err, recordset) {
-
-            if (err) console.log(err)
-
-            // send records as a response
-            console.log(recordset);
-
-        });
-    });
 });
+
+
 
 var server = app.listen(5000, function () {
     console.log('Server is running..');
